a7741ce6a04726613784742a68eece3598998aee
shami <Shamil79797@gmail.com>
2023-03-21 19:09:15 +0300

    build: designed the database

---
+++ database/migrations/2023_03_21_152348_create_codes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+	public function up(): void
+	{
+		Schema::create('codes', function (Blueprint $table)
+		{
+			$table->id();
+			$table->string('title');
+			$table->foreignId('user_id')->constrained('users')->nullable()->cascadeOnUpdate()->cascadeOnDelete();
+			$table->integer('access_id');
+			$table->dateTime('expiration_time')->nullable();
+			$table->foreignId('language_id')->constrained('languages')->nullable()->cascadeOnUpdate()->cascadeOnDelete();
+			$table->timestamps();
+		});
+	}
+
+	public function down(): void
+	{
+		Schema::dropIfExists('codes');
+	}
+};
